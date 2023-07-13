a0817ef226800cb3be7a77be7c0f31b2a2823f08
sim0n89 <52407038+sim0n89@users.noreply.github.com>
2023-07-13 08:20:48 +0300

    что полезного сделано

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,22 +2,45 @@ import telegram
 import os
 from dotenv import load_dotenv
 from os.path import join, dirname
+import random
+import time
+
+
+def get_images_from_folder():
+      images = []
+      for address, dirs, files in os.walk('images'):
+            for name in files:
+                  images.append(os.path.join(address, name))
+      random.shuffle(images)
+      return images
 
 
 def main():
-   dotenv_path = join(dirname(__file__), '.env')
-   load_dotenv(dotenv_path)
-   try:
-         bot_token = os.environ["BOT_TOKEN"]
-   except KeyError:
-         print("Вы не заполнили TOKEN бота")
-         raise SystemExit
-
-   bot = telegram.Bot(token=bot_token)
-   chat_id = -1001614010027
-   # bot.send_message(chat_id=chat_id, text="hello")
-   bot.send_photo(chat_id=chat_id,  photo=open('images/spacex0.jpg', 'rb'))
-
+      dotenv_path = join(dirname(__file__), '.env')
+      load_dotenv(dotenv_path)
+      try:
+            bot_token = os.environ["BOT_TOKEN"]
+      except KeyError:
+            print("Вы не заполнили TOKEN бота")
+            raise SystemExit
+      
+      try:
+            chat_id = os.environ["CHAT_ID"]
+      except KeyError:
+            print("Вы заполнили id канала")
+            raise SystemExit
+      
+      try:
+            sleep_time = os.environ["SLEEP_TIME"]
+      except KeyError:
+            sleep_time = 14400 # 4 часа
+      
+      bot = telegram.Bot(token=bot_token)
+      while True:
+            images = get_images_from_folder()
+            for image in images:
+                  bot.send_photo(chat_id=chat_id,  photo=open(image, 'rb'))
+                  time.sleep(sleep_time)
 
 if __name__ == '__main__':
-    main()
+      main()
